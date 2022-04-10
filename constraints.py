299dc503506477fcc0e0c85df36b063ce17eb0e8
Arya K <aryarksub@gmail.com>
2022-04-10 13:39:37 -0700

    create guess tuple

--- constraints.py
+++ constraints.py
@@ -7,3 +7,10 @@ ConstraintSet = namedtuple("ConstraintSet", ["teams", "confs", "divs",
                                              "positions", "minHt", "maxHt",
                                              "minAge", "maxAge", "minNum",
                                              "maxNum"])
+
+Guess = namedtuple("Guess", ["name", "teamColor", "confColor", "divColor",
+                                "posColor", "htColor", "ageColor", "numColor"])
+
+def updateConstraints(cSet, guess):
+    # Get team, conf, div, pos, ht, age, num of player given name in guess
+    pass
